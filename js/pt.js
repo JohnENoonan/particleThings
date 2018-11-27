bd96815d9f3a96857973ee987cfce74bfdf0caf1
JohnENoonan <noonaj2@rpi.edu>
2018-11-27 00:11:24 -0500

    setup moving vertices

--- js/pt.js
+++ js/pt.js
@@ -1,18 +1,51 @@
 // Source for particleThings
 // work created by Zeana Llamas, Kalvin Janik, and John Noonan
 
-// constants
+// examples
+// https://threejs.org/examples/webgl_points_sprites.html
+// https://www.youtube.com/watch?v=i7DR_Cedbmc
+// https://threejs.org/examples/webgl_interactive_points.html
+// https://threejs.org/examples/webgl_interactive_raycasting_points.html
+//https://threejs.org/examples/webgl_points_dynamic.html
+// https://tympanus.net/Development/3d-particle-explorations/index8.html
+// ui is http://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
+			// https://stemkoski.github.io/Three.js/GUI-Controller.html
+
+// orbit control tumbles
+
+
+// CONSTANTS
 var ASPECT = window.innerWidth / window.innerHeight;
 var FOV = 70; var NEAR = 0.01; var FAR = 10;
 var CONTAINER = document.querySelector('#container');
 
-// globals
+// GLOBALS
+var sys;
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, points;
 
-init();
-animate();
+// CLASSES
+// // data type for one single particle
+// class Particle{
+// 	// constuctor takes: starting position of point,
+// 	constuctor(origPos){
+// 		this.pos;
+// 		this.size = 1;
+// 		this.vel = new THREE.Vector3
+// 		this.restP = origPos;
+// 		this.pos = this.restP;
+// 	}
+//
+// }
+// storage class for many particles
+class Particles {
+	constructor(geo, mat) {
+    this.points = new THREE.Points(geo, mat);
+  }
 
+}
+
+// FUNCTIONS
 function init() {
 
 	camera = new THREE.PerspectiveCamera( FOV, ASPECT,NEAR, FAR );
@@ -21,10 +54,15 @@ function init() {
 	scene = new THREE.Scene();
 
 	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+	// material = new THREE.MeshNormalMaterial();
+	material = new THREE.PointsMaterial({
+            color: "yellow",
+            size: 5,
+            sizeAttenuation: false
+        });
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+	sys = new Particles(geometry, material);
+	scene.add(sys.points);
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -36,9 +74,14 @@ function animate() {
 
 	requestAnimationFrame( animate );
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
-
+	for (var i =0; i < sys.points.geometry.vertices.length; ++i){
+		sys.points.geometry.vertices[i].x += .01;
+	}
+	sys.points.geometry.verticesNeedUpdate = true;
+	// console.log(scene);
 	renderer.render( scene, camera );
-
 }
+
+// EXECUTION
+init();
+animate();
